1307ea0072494ee4abc0043fbedadeeceb5feab2
Mack Peterson <mackpeterson@gmail.com>
2017-05-14 14:33:26 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 var index = require("./index.js")
 
-if (index.name === "Jane") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Jane, Received: "+index.name)
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
