5d77c57cada77cce34b3e8511097674c61ca0979
Zachary Baca <cubsfan.46350@gmail.com>
2017-05-18 19:50:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
-var height = "74"
+var name = "Jane"
+var height = "70"
+if(name === "Jane"){
+  console.log("The name is correct.")
+}
+else {
+  console.log("The name is supposed to be Susan")
+}
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
