4d4315d713981377c6865c97ada72703517ca3de
Dina Dunn <gabumon78@yahoo.com>
2017-05-21 03:48:36 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan")
 }
 
+
+if (height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected the height to be 70")
+}
 // Don't worry about this
 module.exports = { name, height
 }
