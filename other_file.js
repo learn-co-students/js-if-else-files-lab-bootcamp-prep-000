998a1d1b3916d0ff9d7c860b12bff52704a196ba
khadija Bouallou <kb22704@hotmail.com>
2017-05-22 02:10:07 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,15 @@
-console.log(name)
+var name = "Jane"
+var heihgt = "70"
+if (name === "Jane"{
+console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Jane")
+var index = require("./index.js")
+console.log("index.name")
+if (heihgt === "70") {
+  console.log("The height is coorect")
+} else {
+console.log("Expected the height to be 70")
+}
+var index = require("./index.js")
+console.log("index height")
