3213ac4c6ce0614378a2702b424f9b9c2e8f307e
jacob  thompson <jacobtva@gmail.com>
2017-05-31 15:52:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
-var height = "74"
-
+var name = "Jane"
+var height = 70
+if(name === "susan"){
+  console.log('name is correct')
+}
+else {
+  console.log("good job!")
+}
+parseInt(height)
 
 // Don't worry about this
 module.exports = { name, height
 }
-
