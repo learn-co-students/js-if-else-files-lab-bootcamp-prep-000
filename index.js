0d4debac30033dbefeabe195930f63531e25c4c9
Martin Chavez <martin.chavez.mac@gmail.com>
2017-05-31 19:06:10 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = 70
 if (name === "Jane") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Jane")
+    console.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
