3e5ae3dfdb3bae1c261df7dcdfcf23139447c581
Dalya Eidda <dalia.eidda@gmail.com>
2017-06-05 19:20:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
+if (name === "Susan"){
+  console.log("Susan")
+} else {
+  console.log("Expected the name to be Susan")
+}
 
+if (height === "74") {
+  console.log("74")
+} else {
+  console.log("Expected height 74")
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
