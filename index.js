5b2714af3909c50093edaad845d0c0b87283f61f
sungrok choi <sungrok.choi1@gmail.com>
2017-06-09 19:46:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
+var message = `${name} is ${height} inch tall`
 
-
-// Don't worry about this
-module.exports = { name, height
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name th be Susan")
 }
 
+// Don't worry about this
+module.exports = { name, height, message
+}
