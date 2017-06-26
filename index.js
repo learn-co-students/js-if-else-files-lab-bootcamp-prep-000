786c070f2cc4338adce9739c1908a3070862cb04
Ahmadjan Ataniyazov <ataniya@lemoyne.edu>
2017-06-26 00:24:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Jane") {
+  console.log("Expected: Jane");
+} else {
+  console.log("Received: Joe");
+}
+
+if (height === 70) {
+  console.log("Expected: 70");
+} else {
+  console.log("Received: 74");
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
