1eea9091187356800141c652e02eb72b7f05549c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-07-01 17:37:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
-var height = "74"
+var name = "Jane"
+var height = parseInt(70)
+
+if (name === "Jane") {
+  console.log("This is correct")
+} else {
+  console.log("Expected name to be Susan")
+}
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
