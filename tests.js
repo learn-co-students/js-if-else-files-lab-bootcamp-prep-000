35bff52678af5dc42dfd73df57d70d704f7b2732
Asha R <asharoopireddy94@gmail.com>
2017-07-04 22:23:48 +0000

    Done.

--- tests.js
+++ tests.js
@@ -10,6 +10,6 @@ if (index.name === "Jane") {
 
 if (index.height === 70) {
     console.log("The height is correct")
-} else {
+ } else {
     console.log("Expected: 70, Received: "+index.height)
 }
