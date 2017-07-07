9f7c980f90dfebae9added903c4ee729598a8197
Eric Lau <cireual510@gmail.com>
2017-07-07 01:17:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var name = "Joe"
+/*var name = "Joe"
 var height = "74"
-
-
+*/
+var name = "Jane"
+var height = 70
 // Don't worry about this
 module.exports = { name, height
 }
 
+/*if (name === "Susan") {
+  console.log("Well shucks")
+} else {
+  console.log("well it should be susan")
+}
+
+var name = "Susan"
+*/
