9a7866d04c0069c88f6446f9a91dc6efc2b9d45f
Bissan AbuSharar <bissansara@gmail.com>
2017-07-11 04:22:08 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,7 @@
-console.log(name)
+var index = require("./index.js")
+console.log(index.name)
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
