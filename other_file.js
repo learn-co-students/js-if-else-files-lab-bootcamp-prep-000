92ab4aa1b3d090bd16f81e7d7cf09c082521b24d
Chris Rudolph <chrisjames.r@gmail.com>
2017-07-17 20:32:03 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,8 @@
-console.log(name)
+var index = require('./index.js');
+// if( name.name==="Joe") {
+//   console.log("The name is correct");
+// } else {
+//   console.log("Expected the name to be Joe");
+// }
+
+console.log(index.name)
