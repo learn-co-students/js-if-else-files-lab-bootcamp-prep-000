54f024a259b7d9efa4cef34b1228d0550db8fe45
Charlene van Niekerk Niekerk <charlene.runners@gmail.com>
2017-07-18 12:33:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var name = "Joe"
-var height = "74"
-
+var name = "Susan"
+var height = 70
+if(name==="Susan"){
+  console.log("This name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+if(height===70){
+  console.log("This height is correct")
+} else {
+  console.log("Expected height 70")
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
