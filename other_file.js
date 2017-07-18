36207ca44e1afece99cf3a93fc57a1fd078d7afd
Paul Choate <pauldchoate@gmail.com>
2017-07-18 02:25:14 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,7 @@
 console.log(name)
+var index = require("./index.js")
+console.log(index.name)
+
+console.log(name)
+var index =require("./index.js")
+console.log(index.name)
--- tests.js
+++ tests.js
@@ -1,13 +1,12 @@
 var index = require("./index.js")
-
+console.log(index.name)
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
-
-
+console.log(index.name)
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
