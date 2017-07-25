bfd34f1c4757e9bc8487a1d4233ffc36dcefa780
Kevin Kim <kevikim33@gmail.com>
2017-07-25 02:45:58 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,3 +1,17 @@
+/*
+YOUR TURN
+You now know how multiple files interact as well as how if statements work.
+ Now you have to write your code to match some specific tests.
+ Open up tests.js and you will see two if statements.
+ Let's give this a run to start things off by typing nodejs tests.js.
+ You should get two messages:
+Expected: Susan, Received: Joe
+Expected: 70, Received: 74
+Now it's your job to modify index.js so that when you run tests.js it says you got both height and name.
+One note: Capitalization matters and String vs. Numbers matter. Numbers don't have any quotes around them,
+ Strings do have quotes around them!
+*/
+
 var index = require("./index.js")
 
 if (index.name === "Susan") {
