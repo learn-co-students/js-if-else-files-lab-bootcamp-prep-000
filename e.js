33c4e82ac43c309f33d29a9a2855b78c4a33de1c
Niko Stoenescu <nicholas.stoenescu@gmail.com>
2017-07-27 17:47:31 +0000

    Done.

---
+++ e.js
@@ -0,0 +1,13 @@
+var name = "Joe"
+var height = "74"
+
+
+// Don't worry about this
+module.exports = { name, height
+}
+
+if (name === "Susan") {
+  console.log("that name is correct!")
+} else {
+  console.log("Expected name to be Susan")
+}
