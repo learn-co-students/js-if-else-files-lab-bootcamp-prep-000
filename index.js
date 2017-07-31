88c066a153bf862b46a6f110ec843dca1e54bd7e
Alex Borell <alex.borell@yahoo.com>
2017-07-31 00:50:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var name = "Susan"
 var height = 70
 
-if (name === "Susan") {
-    console.log("The name is correct!")
-  }
-else {
-  console.log("Name does not equal Susan")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
