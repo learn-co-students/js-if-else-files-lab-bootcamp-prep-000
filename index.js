4f6e74eb5c3ebd314c68d8a5e7365f8c13c1a3b7
Renso Sanchez <rensosanchez89@gmail.com>
2017-08-05 21:19:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Renso") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Renso")
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
