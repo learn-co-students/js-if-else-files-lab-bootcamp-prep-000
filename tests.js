1725b63803292ad2d03944fcd584dff09eb943ce
Doug Harman <dharman@4harman.com>
2017-08-14 20:31:40 +0000

    Done.

--- tests.js
+++ tests.js
@@ -5,9 +5,6 @@ if (index.name === "Susan") {
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
-
-
-
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
