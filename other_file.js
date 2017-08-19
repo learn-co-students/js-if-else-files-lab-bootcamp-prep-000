58ed30a0d7141596127bd71cdb0b4c3dd72ff2bf
alexchri <arochaix@gmail.com>
2017-08-19 15:44:20 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,13 @@
-console.log(name)
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+if (index.height === "70") {
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 70")
+}
