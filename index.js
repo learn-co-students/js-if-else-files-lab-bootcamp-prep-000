cb32689281cfc4901df53df7f541c09e672542d7
Seoirse Mac an Garda <domhaine.nua@gmail.com>
2017-08-29 07:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var name = "Joe"
 var height = "74"
 
+console.log("Got both, height: " + height + " and name: " + name);
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
