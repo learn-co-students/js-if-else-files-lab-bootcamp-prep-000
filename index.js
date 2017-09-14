824636f03a95c9b78955de36fef74dd5362fd6f9
Franklyn Thompson <frankistone28@outlook.com>
2017-09-14 14:44:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var name = "Susan"
-var height = "74"
+var height = parseInt("70")
 
-if(name === "Susan"){
-  console.log("You got the name");
-}
-
-else if(height === 74){
-  console.log("You got the height");
-}
-
-else{
-  console.log("Expected the name to be Susan");
-}
+// if(name === "Susan"){
+//   console.log("You got the name");
+// }
+//
+// else{
+//   console.log("Expected the name to be Susan");
+// }
 // Don't worry about this
 module.exports = { name, height
 }
