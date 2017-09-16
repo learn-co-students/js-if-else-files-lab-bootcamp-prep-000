18a04e04a34a082dc43a0c5de590564b9d6bee1a
jsedlak65 <james.sedlak@gmail.com>
2017-09-16 05:35:55 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,10 @@
-console.log(name)
+
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+  console.log ("The name is correct")
+}
+
+else {
+  console.log ("Expected the name to be Susan")
+}
