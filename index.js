ee0917decd97a43df910bddd577f0f41dce55fd3
AIT ASSOU <pasarela70@hotmail.com>
2017-09-19 04:22:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var name = "Susan"
-var height = 74
+var height = "70"
 
 if (name === "Susan") {
     console.log("The name is correct")
@@ -7,7 +7,7 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (height === 70) {
+if (height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected the height to be 74")
