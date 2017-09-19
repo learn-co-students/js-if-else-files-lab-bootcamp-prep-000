ee0c012b5481e509e61fe4c2269ec09442e4c52c
Man Leung <lepusleung@gmail.com>
2017-09-19 01:32:59 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -5,11 +5,3 @@ if (index.name === "Susan") {
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
-
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
