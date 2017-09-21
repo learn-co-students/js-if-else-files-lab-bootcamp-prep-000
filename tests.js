66467d0d8e86f7ff10b211962e8b73bd0753b145
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-21 20:49:35 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 var index = require("./index.js")
+var height = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
@@ -7,9 +8,8 @@ if (index.name === "Susan") {
 }
 
 
-
-if (index.height === 70) {
+if (index.height === "74") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 74, Received: "+index.height)
 }
