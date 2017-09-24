2dbc08390e3d6538895141e9ce420be98790d07f
Annette Michaels <micha232@umn.edu>
2017-09-24 07:37:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = 70
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
+//Code for the first part of the lab but removing it makes it require not run this statement
+/*if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log ("Expected the name to be Susan")
+}
+*/
