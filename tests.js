5fe1745861787ff291937422bd1f8a30b3834a2d
WILL IRVIN <william_irvin@live.com>
2017-09-28 22:05:53 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,15 +1,18 @@
-var index = require("./index.js")
+var name = "Joe"
+var height = "74"
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
+
+if (name === "Susan") {
+  console.log("The name is correct")
+}
+else {
+  console.log ("Expected: ");
+  console.log (index.name)
 }
 
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+if (height === "70") {
+  console.log("The height is correct")
+}
+else {
+  console.log("Expected the height to be 70")
 }
