d865f2ba23cc945ef0532ae36f8d4a3e6a2c85f5
Joanne Basa <joannegbasa@gmail.com>
2017-10-01 11:01:34 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var name = "Susan";
 var height = "70";
 
 if(name === "Susan"){
-  console.log("Susan");
+  console.log("The name is correct");
 } else {
   console.log("Expected the name to be Susan");
 }
