8b97764ac782976f59bcc4e3276ce137e339d062
Jonna Leigh <zanycandlecreations@gmail.com>
2017-10-04 23:54:47 +0000

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
+var name =  "Susan" {
+  console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
