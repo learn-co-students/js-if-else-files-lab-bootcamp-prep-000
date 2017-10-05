f386f13da3dc43300bc37a21103de4a83ce428c0
Magnus Skonberg <magnus.skonberg@gmail.com>
2017-10-05 10:29:30 +0000

    Done.

--- tests.js
+++ tests.js
@@ -4,12 +4,9 @@ if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
+  }
+if (index.height === "70"){
+  console.log("The number is correct!")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+  console.log("Expected: 70, Received "+index.height)
 }
