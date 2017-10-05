9a2c2229cac209901a82e2bf33e81d39a60e30a0
Alvin Lu <the.alvinlu@gmail.com>
2017-10-05 04:58:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
-
-// Don't worry about this
-module.exports = { name, height
+if (name == "Susan") {
+  console.log("The name matches")
+} else {
+  console.log("Expected the name to be Susan")
 }
-
+// Don't worry about this
+module.exports = {
+  name,
+  height
+}
