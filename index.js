e4d045973285c6dd15edf6f2e1460fb0b2046c9a
DanceTherapeutics <dancetherapeutics@gmail.com>
2017-10-11 18:01:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var name = "Susan"
-var height = "70"
-if (name  === "Joe") {
+var height = "74"
+if (name  === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expexcted the name to be Susan")
+  console.log("Expected the name to be Susan")
 }
 
 
