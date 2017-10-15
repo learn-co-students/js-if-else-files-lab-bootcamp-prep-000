b028746953ba5a4e1e6843e6a6a132cdaa448b9c
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-15 20:41:10 +0000

    Done.

--- tests.js
+++ tests.js
@@ -2,7 +2,7 @@ var index = require("./index.js")
 
 
 if (index.name === "Susan") {
-    console.log("Susan has no flippin clue if she's doing this right!! Live laugh LOVE :-)")
+    console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
