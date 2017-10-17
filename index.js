d32581460b35365980fb0c24e80d0cf57447c76b
Maverick Tesini <mavericktesini@gmail.com>
2017-10-17 19:23:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan";
+var height = 70;  // removed previous "" around Int (not a String)
 
+/* Redundant seeing this during tests.js file execution. Thus, commenting 'out' for now.
+*  if (name == "Susan") {
+*    console.log("The name is correct.");
+*  } else {
+*    console.log("The name was expected to be Susan, not " + name);
+*  }
+*/
 
 // Don't worry about this
 module.exports = { name, height
 }
-
