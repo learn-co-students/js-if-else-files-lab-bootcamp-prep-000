e69af4c13c5341d8be4069f0397e004ae6f25fe2
Charlie Sparks <csparks@antioch.edu>
2017-10-17 20:01:54 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "Jane") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Jane, Received: "+index.name)
@@ -8,7 +8,7 @@ if (index.name === "Jane") {
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
