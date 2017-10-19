f25cb228b1c0969ae9dcab3799883ea90292f84a
Sofya Verba <sofyaverba@gmail.com>
2017-10-19 01:19:29 +0000

    Done.

--- tests.js
+++ tests.js
@@ -3,13 +3,10 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received: Joe")
 }
-
-
-
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: 74")
 }
