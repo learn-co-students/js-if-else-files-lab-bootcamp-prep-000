d7bcd58c165f4953cc6c4fa8b7c13cf1c3722904
Rajendra Khatri <007rkhatri@gmail.com>
2017-10-27 19:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
-
+if (name === "Susan" && height === "74") {
+  console.log("You got both: " + name + " " + height);
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
