c77b37e3ce5ba6e2d5b586b1d545327a36a691fa
Cavebook <sardine401@gmail.com>
2017-10-30 18:43:17 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,7 +1,2 @@
-var index = require("./index.js")
-
-<<<<<<< HEAD
-console.log(index.name)
-=======
-console.log(index.name)
->>>>>>> 4bc89bd1eb7daf2b3562458a1752ca400a53d6a1
+var index = (require "/index.js")
+console.log(index.height)
