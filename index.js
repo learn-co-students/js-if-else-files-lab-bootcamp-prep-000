4a35907f138510e4c3b860017fd684cf52714f05
Cavebook <sardine401@gmail.com>
2017-10-30 18:37:22 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
+if (height === "70") {
+  console.log("The height is correct")
+}else{
+  console.log("Expected height to be 70")
+}
