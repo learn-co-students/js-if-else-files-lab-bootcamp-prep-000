1cfc4cd174c79caa682c9cf1dc20ffd04af86d03
Dan Drumwright <danieldrumwright@gmail.com>
2017-11-14 04:13:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height= "70"
+
+if  (name==="Susan") {
+  
+  console.log ("The name is correct")
+} 
+else {
+  
+  console.log ("expected the name to be Susan")
+}
+
+
+var name = "joe"
+var height= "74"
+
+if (name==="joe") {
+  
+  console.log ("The name is correct")
+} else {
+  
+  console.log ("expected the name to be joe")
+}
+
+
 
 
 // Don't worry about this
