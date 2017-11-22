5d269f65c46e25609ed152fb597c76a9ab33863f
Trịnh Bảo Khai Nguyên <14520614@gm.uit.edu.vn>
2017-11-22 14:26:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
-
+if(name==="Susan"){
+  console.log("the name is correct")
+}
+else{
+  console.log("Expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
