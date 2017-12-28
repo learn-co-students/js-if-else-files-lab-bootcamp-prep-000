4d51cfeadbee8223c9c02e14b32b303ac3a8ca68
tunde Oretade <tundeiness@yahoo.co.uk>
2017-12-28 00:12:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
+if(name === "Susan"){
+  console.log("You got the  name correct ")
+}else{
+  console.log("you missed the name")
+}
+
+if(height === "74"){
+  console.log("You got the height")
+}else{
+  console.log("you missed the height")
+}
 
 // Don't worry about this
 module.exports = { name, height
