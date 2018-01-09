8ab1153a219ddf05a87bfa54f26a47c3ef8de568
Gary Morford <morfordg@mst.edu>
2018-01-09 05:19:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
-// Don't worry about this
+//don't worry about tbjs/ Do// Don't worry about this
 module.exports = { name, height
-}
-
-if (name === "Susan") {
-  console.log("Name is correct")
-}else{
-  console.log("expected the name to be Susan")
 }
