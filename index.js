cc804562dacca939724159b8ac76e81fa43fa72c
Muhammad Tahir Khan <muhammadtahirk@gmail.com>
2018-01-12 06:00:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan";
+var height = 70;
 
+/*
+if (name === "Susan") {
+  console.log("Name match found!");
+} else {
+  console.log("Sorry, the name doesn't match!");
+}
+*/
 
 // Don't worry about this
 module.exports = { name, height
