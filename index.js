3be8ffe9c61180a623f23540b2a239d7c0448883
kpgurmule@gmail.com <kpgurmule@gmail.com>
2018-01-12 18:48:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var name = "Joe"
 var height = "74"
 
+if height==70 {
+  console.log("The height is correct")
+}
+else {
+  
+  console.log("The height is incorrect")
+}
+
+if name==Susan {
+  console.log("The name is correct")
+  
+
+}
+else{
+  console.log("The name is incorrect")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
