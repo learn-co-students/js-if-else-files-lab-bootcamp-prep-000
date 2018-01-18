c78ffe253f4a7ac341cc43a06731650a98b81fdc
Lawrence Gee <lshameless@gmail.com>
2018-01-18 22:56:47 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,20 +1,13 @@
-var index = ("./index.js")
+var index = ("./tests.js")
 
 var Name = "Susan"
-var height = "70"
+var Height ="70"
 
-if (name === "Susan") {
+
+if (Name === "Susan") {
     console.log("The name is correct")
-} 
-
-else {
-  console.log ("Name is not correct")
 }
-
-{ 
-if else {
-    console.log("Height is not correct")
-}
-
-if (index.height === 70) {
+ 
+ if (Height === "70") {
     console.log("The height is correct")
+}
