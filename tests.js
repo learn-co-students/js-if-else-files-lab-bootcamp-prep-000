bd6c9a492fe71f5361497fc74cf9e2a8e893f834
Douglas Uretsky <douglasuretsky@gmail.com>
2018-02-06 22:54:50 +0000

    Done.

--- tests.js
+++ tests.js
@@ -6,9 +6,9 @@ if (index.name === "Susan") {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
+https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/programming-js-in-the-ide/practicing-conditional-statements#
 
-
-if (index.height === 70) {
+if (index.height === '74') {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
