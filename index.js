657b0cdb42af7c3827f1fa40de25b119ba5c9391
gsmcginnis1990@gmail.com <gsmcginnis1990@gmail.com>
2018-03-04 01:38:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
+var index = require("./index.js")
+if (name==="Susan") {
+    console.log("The name is correct")
+  } else {
+  console.log("Expected the name to be Susan")
+}
 
 
 // Don't worry about this
