ccc17b8e97525b35e50ec7de831a97f924da3b94
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-09 21:16:32 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ else {
   console.log("Expected the name to be Susan")
 }
 
+if (height === "70") {
+  console.log("The height is correct")
+}
+else {
+  console.log("Expected the height to be 70")
+}
+
+
 // Don't worry about this
 module.exports = { name, height
 }
