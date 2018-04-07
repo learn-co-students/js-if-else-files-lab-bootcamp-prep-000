432a123f33787b19121077caad78e63364727cc0
Jessica Green <jgreen629@yahoo.com>
2018-04-07 19:09:11 +0000

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var height = "70"
 if (name === "Susan") { 
   console.log("The name is correct") }
 else {
-  console.log("Expected: Susan, Received  " + index.name)
+  console.log("Expected: Susan, Received  " + name)
 }
 
 if (height === 70) {
   console.log("The height is correct") }
  else {
-  console.log("Expected: 70, Received  " + index.height) 
+  console.log("Expected: 70, Received  " + height) 
 }
 // Don't worry about this
 module.exports = { name, height
--- tests.js
+++ tests.js
@@ -3,7 +3,7 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+name)
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
@@ -11,5 +11,5 @@ if (index.name === "Susan") {
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+height)
+    console.log("Expected: 70, Received: "+index.height)
 }
