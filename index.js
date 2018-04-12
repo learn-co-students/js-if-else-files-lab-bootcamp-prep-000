b65f2fbc222c051717d4e76418e177102edd011a
Fe Geiger <feo.geiger@web.de>
2018-04-12 17:27:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var name = "Joe"
 var height = "74"
-
-
+if (name === "Susan") {
+  name = name
+}
+else {
+  name = "Susan"
+}
+if (height === 70) {
+  height = height
+}
+else {
+  height = 70
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
