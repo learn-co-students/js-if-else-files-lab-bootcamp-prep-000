5c18a89dc855ec854ca88310890ae47c4e9cc5c0
ANIL KUMAR PILLI <kanil6608@gmail.com>
2018-04-16 18:30:48 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
-var index = require("./index.js")
+var index.name = "Susan"
 
-if (index.name === "Susan") {
+if (index.name=== "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
