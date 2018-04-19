66f9bc780c9bb5f557c5da6c2ce201eeceec78a2
aaron bartrail <bartrailz@gmail.com>
2018-04-19 00:25:45 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 var index = require("./index.js")
-
+console.log(index.name)
+console.log(index.height)
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
@@ -8,7 +9,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
