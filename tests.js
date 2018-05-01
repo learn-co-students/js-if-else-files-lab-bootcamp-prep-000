841bf752384975c83c68eedb08cb665ee7bc92d1
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-01 16:52:40 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,15 +1,10 @@
-var index = require("./index.js")
+var name = "Susan"
+var height = "74"
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
+if (name === "Susan") {
+  console.log("The name is correct")
 }
 
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+if (height === "74") {
+  console.log("The height is correct")
 }
