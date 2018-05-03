6970799e98499f1578c21958aa396f398c11a47c
Scott Kinsella <dj7kinsella@aol.com>
2018-05-03 00:10:05 +0000

    Done.

--- tests.js
+++ tests.js
@@ -2,14 +2,12 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
+} else { console.log("expected the name to be susan")
 }
 
 
 
 if (index.height === 70) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+} else { console.log("expexted the height to be 70")
 }
