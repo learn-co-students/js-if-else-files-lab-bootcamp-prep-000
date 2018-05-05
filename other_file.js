d20636bcd74224884625a9ba5d08097bcb4c60b1
nmkleiner@gmail.com <nmkleiner@gmail.com>
2018-05-05 19:30:20 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,12 @@
-console.log(name)
+var index = require('./index.js')
+console.log(index)
+
+//check name
+if (index.name === 'Joe') {
+  console.log('The name is correct')
+}
+
+//check height
+if (index.height === '74') {
+  console.log('The height is correct')
+}
