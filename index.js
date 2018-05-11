2f79bba7986d3a8979b5b7dd5ee864353f8f3bbe
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-05-11 10:19:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
+if (name = "Susan") {
+    console.log("Susan")
+}
+
+if (height = 70) {
+  console.log(70)
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
