6202f5389eb048d0d552246f0740d01381d6be67
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-05-11 10:22:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var name = "Susan"
 var height = "70"
 
-if (name = "Susan") {
-    console.log("Susan")
+if (name == "Susan") {
+    console.log("The name is correct")
 }
 
-if (height = 70) {
-  console.log(70)
+if (height == 70) {
+  console.log("The height is correct")
 }
 
 // Don't worry about this
