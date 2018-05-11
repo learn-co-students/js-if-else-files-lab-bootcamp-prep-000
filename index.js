3f57be4003b0c59cf153a8abdf5a4fc5fdb9dcb0
Hannah Kang <hkang92@gmail.com>
2018-05-11 22:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-var name = "Susan"
-var height = "70"
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (index.height === "70") {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
+var name = "Joe"
+var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
+
+
