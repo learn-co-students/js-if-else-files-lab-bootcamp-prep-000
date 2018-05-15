1fd2c2aaf214b1a6993b072308b98c2de9381f1a
Houssein Abil <milameet@gmail.com>
2018-05-15 04:30:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var name = "Jane"
-var height = "70"
+var name = "Susan"
+var height = 78
 
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
+}
+
+if (height === 78) {
+  console.log("The height is correct")
+} else {
+  console.log("Expected height to be 78")
 }
 
 
--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
 var index = require("./index.js")
 
-if (index.name === "Jane") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Jane, Received: "+index.name)
+    console.log("Expected: Houssein, Received: " + index.name)
 }
 
 
 
-if (index.height === 70) {
+if (index.height === 78) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 78, Received: "+ index.height)
 }
