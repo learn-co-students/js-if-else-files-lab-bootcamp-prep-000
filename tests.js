dd8a3a88dce6adec471a1aa43b0362ae39086ab7
Romana Akter <urmee04sust@gmail.com>
2018-05-15 17:19:33 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
-var index = require("./index.js")
+var index = require("./index.js");
 
 if (index.name === "Susan") {
-    console.log("The name is correct")
+    console.log("The name is correct");
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received: "+index.name);
 }
 
 
 
-if (index.height === 70) {
-    console.log("The height is correct")
+if (index.height === 74) {
+    console.log("The height is correct");
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: "+index.height);
 }
