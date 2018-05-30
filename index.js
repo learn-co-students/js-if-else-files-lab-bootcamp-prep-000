f574dd5cc28ccff4f2732acd5f76fa0c3ca5f4c1
Cassandra Montoya <montoya.c@outlook.com>
2018-05-30 18:45:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+if (name === "Susan"){
+  console.log ("The name is correct")
+} else {
+  console.log ("Expected the name to be Susan")
+}
+
+
+var height = 70
+if (height === 70){
+  console.log ("The height is correct")
+  
+}else {
+  console.log ("Expected the height to be 70")
+}
 
 
 // Don't worry about this
