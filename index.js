57a6016454d26177dc44ba4b195520cade498adb
Tom Lee <tom.kennedy.lee@gmail.com>
2018-05-31 10:11:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 var name = "Susan"
-var height = "70"
+var height = parseInt(70)
 
 
 // Don't worry about this
 module.exports = { name, height
-}
-if (height === "70") {
-  console.log("The height is correct")
 }
--- other_file.js
+++ other_file.js
@@ -1,4 +1,3 @@
 var index = require("./index.js")
 
 console.log(index.name)
-console.log(index.height)
