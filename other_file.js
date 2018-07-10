90468ade27937ad47587b26f7d14514eb73d5a04
braj011 <bhagyaraj01@gmail.com>
2018-07-10 12:57:59 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,7 @@
-console.log(name)
+var index = require("./index.js")
+
+/* ^^ tells JS to load index.js file in and put its contents inside of the 
+index variable within other_file.js. 
+*/ 
+
+console.log(index.name)
