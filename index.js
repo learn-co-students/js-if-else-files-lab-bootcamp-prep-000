e416cfe8e96700e0a28da6442240bd2a8fd52a61
christian.cook91@outlook.com <christian.cook91@outlook.com>
2018-07-10 12:20:31 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan")
 }
 
-if (height === "70") {
+if (height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected the height to be 70")
