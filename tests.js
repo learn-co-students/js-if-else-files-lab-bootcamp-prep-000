594edb97f87d2eabc3b0fab15e977dc4ca86c2a2
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-10 22:39:06 +0000

    Done.

--- tests.js
+++ tests.js
@@ -8,8 +8,8 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === "74") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 74, Received: "+index.height)
 }
