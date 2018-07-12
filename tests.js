e437ac8464384f9fea8de6d8b0eb66aa91c9c26b
Stefanie Torossian <stefanietorossian@gmail.com>
2018-07-12 19:31:11 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
 var index = require("./index.js")
 
+//Name
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received:Joe")
 }
 
-
-
-if (index.height === 70) {
+// Height
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: 74")
 }
