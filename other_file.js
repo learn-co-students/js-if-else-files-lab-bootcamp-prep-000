b870326dbcd2480d1b0e3468548338838ea0ef71
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-14 22:02:37 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,8 @@
-console.log(name)
+
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
