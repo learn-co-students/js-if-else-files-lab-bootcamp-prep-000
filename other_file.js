e36d3f866a21322e67f778d882431d191aeff16f
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-02 07:10:57 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,7 +1,2 @@
 var index = require("./index.js")
-console.log(name)
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}
+console.log(index.names)
