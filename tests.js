f8f60ccd90970f1f85d92eeb0783b8a1f860d9aa
Christopher Anderson <andersonchris227@gmail.com>
2018-08-15 22:11:01 +0000

    Done.

--- tests.js
+++ tests.js
@@ -2,7 +2,9 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} else {
+} else if (index.name = "Joe") {
+    console.log("The name is correct "+index.name)
+}else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
@@ -10,6 +12,8 @@ if (index.name === "Susan") {
 
 if (index.height === 70) {
     console.log("The height is correct")
-} else {
+} else if (index.height = 74) {
+    console.log("The height is correct "+index.height)
+}else {
     console.log("Expected: 70, Received: "+index.height)
 }
