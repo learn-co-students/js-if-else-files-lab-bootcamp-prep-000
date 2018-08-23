c2c3c916390088cbc0b44ce9bd591bb1936fbad2
Noah Freedley <nfreedley@gmail.com>
2018-08-23 12:05:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var name = "Joe"
+varname = "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 var height = "74"
 
 
@@ -6,3 +10,8 @@ var height = "74"
 module.exports = { name, height
 }
 
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
