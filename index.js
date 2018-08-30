63ff4910b09632375874eabc57023a4e8199daf9
Mielyn Acosta <mielyndl@gmail.com>
2018-08-30 22:03:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+var name = "Susan";
+var height = "74";
 
-var name = "Susan"
-var height = "74"
 
 
 // Don't worry about this
 if (name === "Susan") {
     console.log("The name is correct")
-}else {
-  console.log("Expected the name to be Susan")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+if(height === "74"){
+  console.log("The height is correct")
+} else{
+  console.log("Expected the name to be Joe")
 }
--- other_file.js
+++ other_file.js
@@ -1,2 +1,3 @@
 var index = require("./index.js")
 console.log(index.name)
+console.log(index.height)
