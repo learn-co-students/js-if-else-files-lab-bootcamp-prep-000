5fb2c98a24d110f298652233a36c9b3bf237f1d4
adelecalvo@gmail.com <adelecalvo@gmail.com>
2018-09-02 18:45:00 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,4 @@
-console.log(name)
+var index = require("./index.js")
+// This tells javascript to load the index.js file and put its contents inside of the index variable within other_file.js.
+
+console.log(index.name)
