1f4d0cd470c9b05619352bee0c4aae0b56e67721
Nasser Sanou <nassersanou23@gmail.com>
2018-09-14 21:18:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan";
+var height = "70";
+
+if ( name === "Susane") {
+  console.log("The name is correct")
+} else {
+  console.log (`expected the name yo equal ${name}`)
+} ;
+if ( height === "70") {
+  console.log("The height is correct")
+} else {
+  console.log (`expected the name yo equal ${height}`)
+} 
 
 
 // Don't worry about this
