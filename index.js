e9f3852bb74917934afd9abbcfe40523a868db3b
elisa stater <elisa_60@msn.com>
2018-10-04 04:37:41 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var name = "Joe"
 var height = "74"
 
 
-// Don't worry about this
-module.exports = { name, height
+if (name === "Joe") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
 }
 
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
+if (height ==="74") {
+  console.log("The height is correct")
+}
+else {
+  console.log("Expected the height to be 70")
 }
