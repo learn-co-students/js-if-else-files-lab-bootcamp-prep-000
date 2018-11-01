2408bc087ec2dea09a271dfe09f6cef59b729709
achilleas.loumis P. Loumis <achilleas.loumis@gmail.com>
2018-11-01 14:55:57 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan"
      console.log("The name is correct")
-else {console.log("Expected the name to be joe")}
+else {console.log("Expected the name to be Susan")}
 if (height === "74") {console.log("The height is correct")}
 
 
