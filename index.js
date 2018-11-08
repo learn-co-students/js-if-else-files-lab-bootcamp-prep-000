fa8a48454e57ac42b2840fcac37c0d1c654416c4
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-08 19:33:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 
-if name === "Susan") {
+if (name === "Susan") {
   console.log ("The name is correct")
 } else {
   console.log("Expected the name to be Susan")
