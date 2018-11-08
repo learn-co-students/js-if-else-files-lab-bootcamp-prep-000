de46e6efc308aca0fbbc27821dcf0aadc20bc68b
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-08 19:52:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var name = "Gwynevere"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 5)
+console.log(height + 5)
 
 // Don't worry about this
 module.exports = { name, height
