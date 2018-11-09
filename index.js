63fb12f5930217639e39c19543f33714e554e9c7
Fei Qi <feiqi9047@gmail.com>
2018-11-09 06:29:07 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ var height = "74"
 module.exports = { name, height
 }
 
+if (name === "Susan") {
+    console.log("The name is correct")
+}
+
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+var name = "Susan"
+
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+var heigh = "74"
+
+if (height === "74") {
+    console.log("The height is correct")
+} else {
+    console.log("70")
+}
