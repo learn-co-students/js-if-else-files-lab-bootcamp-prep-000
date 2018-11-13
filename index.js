11b51d3c257fe235416af63ca2d9ab8d74dcdd03
gt3ch05 <gt3ch05@gmail.com>
2018-11-13 02:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ var height = 70
 // Don't worry about this
 module.exports = { name, height
 }
-
-if(name === "Susan")
-  { console.log("That is the correct name")
-}  else {
-    console.log("That is the incorrect name")
-}
