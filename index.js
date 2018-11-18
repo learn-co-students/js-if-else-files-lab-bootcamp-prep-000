6374a149cabe5a15b2590a4429a345bdcf4dd21d
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-18 16:32:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var name = "Joe"
-var height = "74"
+// **Hi! This code still have issues. "Susan" keeps appearing
+// at the bottom :) Submitted it but still going back to check
+// this code soon
 
-
+var index = require("./tests.js")
+var name = "Susan"
+var height = 70
+ 
 // Don't worry about this
 module.exports = { name, height
 }
 
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+height)
+}
+
+
+
+if (height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+height)
+}
