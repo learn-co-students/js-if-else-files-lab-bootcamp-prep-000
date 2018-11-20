038bfaffd3b770274fd5fb2025e212ab9c117500
Anders Hornor <anders.hornor@gmail.com>
2018-11-20 02:12:27 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var index = require("./index.js")
 index.name = "Susan"
 index.height = 70
+// The two previous lines seem like a copout...Im going to review some preious lessons as I believe that they covered how to change a variable in another file from ones current file by referenceing it in a particular way.
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
