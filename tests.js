68d00354e9c4df4622fea824e3aef3de1c5db408
Vytautas Klimavicius <vyklimavicius@gmail.com>
2018-11-22 17:01:54 +0000

    Done.

--- tests.js
+++ tests.js
@@ -2,14 +2,16 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
+}
+else {
+    console.log("Expected: Susan, Received: " + index.name)
 }
 
 
 
 if (index.height === 70) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+} 
+else {
+    console.log("Expected: 70, Received: " + index.height)
 }
