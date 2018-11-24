a9a7afc2c65b665e5c22e908036e65c06fe0c623
Abu Islam <thebooming1992@gmail.com>
2018-11-24 00:47:57 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,15 +1,19 @@
-var index = require("./index.js")
+var index = require("./index.js");
+ 
+var index_Name = index.name;
+var index_Height = index.height;
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
+
+if (index_Name === "Susan") {
+    console.log("The name is correct");
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received: "+ index_Name);
 }
 
 
 
-if (index.height === 70) {
-    console.log("The height is correct")
+if (parseInt(index_Height) === 70) {
+    console.log("The height is correct");
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: "+ index_Height);
 }
