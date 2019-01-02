a35f404f3ded54132b568b5cc700875f767d9744
Brandon Teich <brandonteich123@gmail.com>
2019-01-02 00:48:22 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var name = "Susan"
 if (name === "Susan") {
   console.log("The name is correct")
   }
-  var height = "70"
- if (height === "70") {
-     console.log("The height is correct")
+var height = "70"
+if (height === "70") {
+  console.log("The height is correct")
  } 
