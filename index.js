675784280d26d40b45304f87f4648df38b0eaa29
Brandon Teich <brandonteich123@gmail.com>
2019-01-02 00:47:20 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var height = "74"
 module.exports = { name, height
 }
 
+var name = "Susan"
+if (name === "Susan") {
+  console.log("The name is correct")
+  }
+  var height = "70"
+ if (height === "70") {
+     console.log("The height is correct")
+ } 
