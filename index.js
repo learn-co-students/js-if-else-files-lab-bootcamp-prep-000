6decde0ed4262c18c5ce0f1e3bc0c1c71f5d1ab3
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-13 15:07:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var name = "Susan";
-var height = "70";
+var height = "74";
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
-if(name=== "Susan"){
-  console.log("The name is correct");
-}else{
-  console.log("Expected the name to be Susan")
-}
+
