09d587a90b766d250d1fd52cffba545c5a0c352e
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-15 19:54:00 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,3 +1,2 @@
-var index= require("./index.js")
-
-console.log(index.name)
+var index= require("./index.js");
+console.log(index.name);
--- tests.js
+++ tests.js
@@ -8,7 +8,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === "74") {
+if (index.height === 70) {
     console.log("The height is correct");
 } else{
        console.log("Expected 70, but Received: "+index.height);
