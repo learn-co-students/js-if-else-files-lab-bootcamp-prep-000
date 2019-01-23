421cd7983b4e42e8b03de7a6d5bc143565e30bd6
Kash Reid-Bashir <kash@reid-bashir.com>
2019-01-23 14:06:56 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Susan") 
+{
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -8,7 +9,8 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === 70)
+{
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
