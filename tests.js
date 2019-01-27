4148010ee5404858abfc012be5461dca753dc2a9
remi mendoza <mendozaremi@gmail.com>
2019-01-27 18:07:09 +0000

    Done.

--- tests.js
+++ tests.js
@@ -8,8 +8,8 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === "ten") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: ten, Received: "+index.height)
 }
