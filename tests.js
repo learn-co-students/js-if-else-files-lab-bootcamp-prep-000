dd02ed3d76a9d83c522e497f69f28b9361e46fa7
antonio_xray V Tobar Jr <antonio_xray@hotmail.com>
2019-02-07 03:08:54 +0000

    Done.

--- tests.js
+++ tests.js
@@ -2,14 +2,19 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
+} 
+else if (index.name === "Joe") { console.log("The name is correct")
+  
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
 
 
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
+} 
+else if (index.height === "74") {console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
