d0a492546bd23e928755d92af58ae69ea1c3c897
Ricky Coder <kingupper0@gmail.com>
2019-02-18 10:02:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var name = "Joe"
-var height = "74"
+var name = "Programmer"
+var adress = "Petion Ville"
+var height = 88
 
 
+console.log(name);
 // Don't worry about this
-module.exports = { name, height
+module.exports = { name, height, adress
 }
 
