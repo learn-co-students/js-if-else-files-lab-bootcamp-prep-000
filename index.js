8dfb92520a57b68dd8fed2e97a8d0d4e2b4fd5d4
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-29 01:54:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var name = "Joe"
-var height = "74"
-
-
+var name = "Susan"
+var height = 70
+if (name==="susan"){
+  console.log("name is susan")
+} else {
+  console.log("name isn't susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
