d918f1e99a7d2d96c75b3703469e5bd4b3d633a4
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-03-31 20:33:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var name = "Joe";
-if (name === "Susan") {
+  if (name === "Susan") {
     console.log("The name is correct")
+}
+   else {
+    console.log("Expected the name to be Susan")
 }
 var height = "74";
 
