5dd86b510cce540d8a7838f09c7ba929ea23ae2b
Alexander Schelchere <ascellie@gmail.com>
2019-04-20 19:25:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 var name = "Joe"
 var height = "74"
 
+name = "Susan";
+height = 70;
+// console.log(name)
+// // Positive test
+// let okay = (str) => {
+//   // initialize the item. This is what we'll be returning.
+//   let item;
+//   switch(str) {
+//     case name:
+//       item = console.log(`${str} is the correct name!`);
+//       break;
+//     case height:
+//       item = console.log(`${str} is the right height!`);
+//       break;
+//     default:
+//     item = console.log('Whoops! that isn\'t a name or a height!');
+//       break;
+//   }
+//   console.log("The name is correct");
+// }
+// // negative test
+// let noWay = (str) => {
+// let item;
+// // based on the input, produce a string for the expected height or name.
+//   switch(str) {
+//     case name:
+//       item = console.log(`Expected Susan, Received ${str}`);
+//       break;
+//     case height:
+//       item = console.log(`Expected 70, Received ${str}`);
+//       break;
+//     default:
+//       item = console.log('Whoops! that isn\'t a name or a height!');
+//       break;
+//   }
+  
+//   return item;
+// }
+
+// name === "Susan" ? okay(name) : noWay(name)
+// Number(height) === 70 ? okay(height) : noWay(height)
+
 
 // Don't worry about this
 module.exports = { name, height
