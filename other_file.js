f5d22a54deb742edf190ef761efbc478c727380b
Elsa Ryynänen <elsaryynanen@outlook.com>
2019-04-22 12:05:14 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,13 @@
-console.log(name)
+var index = require("./index.js")
+
+console.log(index.name)
+
+console.log(index.height)
+
+
+if (name ==="Susan") {
+  console.log("The name is correct")
+}
+if(height ==="74") {
+    console.log("The height is correct")
+  }
