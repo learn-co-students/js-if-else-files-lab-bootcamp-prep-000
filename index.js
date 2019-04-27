0847188a92180d98394523ff18038e4a30646fec
Gufwat Antiphas <antiphas4christ888@gmail.com>
2019-04-27 03:31:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
-
+if (name === "Susan"){
+  console.log("the name is correct")
+}else{
+  console.log("expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
