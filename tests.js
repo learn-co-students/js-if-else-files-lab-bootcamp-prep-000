41a8782ebf8959a913d7fa8be2b517aa3b1436f0
Summer Duncan-Clarke <summer_duncan5@yahoo.com>
2019-05-14 13:42:52 +0000

    Done.

--- tests.js
+++ tests.js
@@ -3,7 +3,7 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received: "+ index.name)
 }
 
 
