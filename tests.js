ea479a20233b550aebf0706b4a97f83f3e856839
Laronda Bell <larondak.bell@gmail.com>
2019-07-15 01:06:14 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name == "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -8,8 +8,8 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
-    console.log("The height is correct")
+if (index.height == 70) {
+    console.log("The height is correct");
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
