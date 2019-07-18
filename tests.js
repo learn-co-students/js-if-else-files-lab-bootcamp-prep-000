d95ab1a0f984ec288af76ce76559063433f43be6
Priscilla Firpo <priscillayf@gmail.com>
2019-07-18 03:53:48 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
+var index = require("./index.js")
 
-
-if (index.height === 70) {
+if (index.height === "74") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
