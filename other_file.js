a9532e9092ec8297a26873c8b6431b13daa5dd26
Imran Iqbal <imran1337@gmail.com>
2019-08-01 00:26:33 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,9 @@
-console.log(name)
+var index = require("./index.js")
+
+console.log(index.name)
+
+if (index.name === "Susan") {
+  console.log("Was expecting Joe") 
+}   else {
+    console.log("That's correct!") 
+ }
