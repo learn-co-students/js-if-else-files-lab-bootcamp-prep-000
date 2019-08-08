b95d76e9d850f127344e832c2a999bd4d5fc00c9
TShipley <taneymail@gmail.com>
2019-08-08 16:23:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var name = "Joe"
+var name = "Susan";
 var height = "74"
 
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log ("Expected the name to be Susan")
+}
+
+if (height = "74") {
+  console.log ("The height is correct")
+} else {
+  console.log ("Expected the height to be 74")
+}
 
 // Don't worry about this
 module.exports = { name, height
--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 var index = require("./index.js")
 
+
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
@@ -8,7 +9,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === "74") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
