d4ff3f8f23f9b95b55ed4b441bdcdd1f6d92e560
Amy Tran <amy.le.1026@gmail.com>
2019-10-22 04:34:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
+height = parseInt(height);
 
+if (name != "Susan"){
+   name = "Susan";
+}
+
+if (height != 70){
+  height = 70;
+}
 
 // Don't worry about this
 module.exports = { name, height
