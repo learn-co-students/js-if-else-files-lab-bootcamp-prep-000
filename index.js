5d5490756d4cce0795b94773b350e73a5862dd19
Adrian Orive <aorive07@gmail.com>
2019-12-02 00:10:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74";
 
-if (name === "Susn")
+if (name === "Susan")
 {
   conosole.log("The name is correct");
+  } else {
+    console.log("Ex[ected the name to br Susan");
+    
   }
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 
