1f2c957560fb5bf615cf299aaba7f5a19d2b48fc
James Jensen <jnjensen@live.com>
2020-02-02 05:16:43 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,15 @@
-console.log(name)
+var index = require("./index.js")
+
+console.log(index.name)
+
+if (index.name === "Susan"){
+  console.log("The name is correct")
+} else {
+  console.log("Expected: Susan, Recieved: " + index.name)
+}
+
+if (index.height === "70"){
+  console.log("The height is correct")
+} else {
+  console.log("Expected: 70, Recieved: " + index.height)
+}
