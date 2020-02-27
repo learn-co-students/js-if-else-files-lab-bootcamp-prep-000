a0abd4d152c7eebf29e1d9b9a9680f518431c6e3
Sergio A. Martinez <rednblue1995@gmail.com>
2020-02-27 23:56:03 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+if (name === "Susan") { console.log(
+  "The name is correct") }
+  if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
 
---
+++ js/other_file.js
@@ -0,0 +1,3 @@
+console.log(name)
+var index = require("./index.js")
+console.log(index.name)
