2652349a8f6be0f2b75b20cc5dcae1b8b29a5a23
Annie T Bellefleur <anniebellefleur@gmail.com>
2020-03-02 14:39:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var name = "Susan"
-var height = 70
+var name = "Susan";
+var height = "70";
 
-
+if (name === "Susan"){
+  console.log("The name is correct")
+} else if (name === "Lauren"){
+  console.log("This is her secretary")
+} else {
+  console.log("The name should be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
