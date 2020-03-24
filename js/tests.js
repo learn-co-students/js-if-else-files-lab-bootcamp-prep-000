e3138853b22e8c754a626afb33d489c98a904854
practical-kernel-3029 <nah1222@yahoo.com>
2020-03-24 00:35:55 +0000

    Done.

--- js/tests.js
+++ js/tests.js
@@ -4,12 +4,17 @@ if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
+  
 }
 
-
-
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: height, Received: "+index.height)
+  
 }
+
+
+
+
+
