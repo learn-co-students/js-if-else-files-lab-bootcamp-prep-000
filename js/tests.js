6cb252f13b29aa5a9329bf10a652ece12d99ef46
Richard Lucero <lucero0208@yahoo.com>
2020-05-10 06:25:33 +0000

    Done.

--- js/tests.js
+++ js/tests.js
@@ -1,15 +1,7 @@
-var index = require("./index.js")
+var name = 'Susan';
+var height = 70
+if(name==="Susan"){console.log("The name is correct")} 
+else{console.log("Expected the name to be Susan")}
+if(height === 70){console.log("The height is correct")} 
+else{console.log("Expected the height to be 70")}
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
