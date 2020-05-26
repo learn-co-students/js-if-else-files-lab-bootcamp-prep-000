3e0e0c9b52bea666e2f020f2eb9c730955671654
ra1nrouge <jsdst11@mail.rmu.edu>
2020-05-26 02:25:36 +0000

    Done.

--- other_file.js
+++ other_file.js
@@ -1,1 +1,15 @@
 console.log(name)
+var index = require("./index.js")
+var index = require("./index.js")
+
+console.log(index.name)
+
+var index = require("./index.js")
+
+console.log(index.name)
+
+
+var index = require("./index.js")
+var index = require("./index.js")
+
+console.log(index.name)
