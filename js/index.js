10301ff68fc42fc28c46789a895b6c1486bfcffd
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-06 19:02:00 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -4,4 +4,9 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+if(name==="susan"){
+  console.log("the name is correct")
+} else{
+  console.log("expected the name to be susan")
+}
 
