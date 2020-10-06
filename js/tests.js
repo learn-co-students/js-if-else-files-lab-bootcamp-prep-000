bb5f0a5f3eb14ddcffd2096dfe2753dcae68a0c5
Bill Miles <williammiles201@gmail.com>
2020-10-06 19:23:48 +0000

    Done.

--- js/tests.js
+++ js/tests.js
@@ -5,10 +5,7 @@ if (index.name === "Susan") {
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
-
-
-
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
