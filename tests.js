59f89226cd79371c61a04caf2b63f1035dd511f6
breezy-constructor-6081 <murtaza63@gmail.com>
2020-10-24 08:24:52 +0000

    Done.

--- tests.js
+++ tests.js
@@ -13,3 +13,15 @@ if (index.height === 70) {
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
+
+
+if(index.name === "Joe"){
+  console.log("The name is correct")
+}else{
+  console.log("Name is not correct");
+}
+if(index.height>70){
+  console.log("The height is correct")
+}else{
+  console.log("The height is not correct");
+}
