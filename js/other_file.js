00b6f7e7f311d09c1802d812a0f1e4a62d5cd8a1
dandy-script-7191 <021hansolo120@gmail.com>
2020-11-11 03:30:17 +0000

    Done.

--- js/other_file.js
+++ js/other_file.js
@@ -1,3 +1,15 @@
 var index = require("./index.js")
- 
-console.log(index.name)
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
+
+
+
+if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
