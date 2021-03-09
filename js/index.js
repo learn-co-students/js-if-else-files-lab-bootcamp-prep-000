76de25f02ad98c78cf5a3099720f0fa34c4e2c17
celestial-applet-1261 <zihan991@gmail.com>
2021-03-09 03:33:21 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-var name = "Joe"
-var height = "74"
-
-// Don't worry about this
-module.exports = { name, height
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
 }
 
