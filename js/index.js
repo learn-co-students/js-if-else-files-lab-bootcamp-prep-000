64f9bd99c712276f4fc90c0e02ac8742c54b5461
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-05-02 18:06:29 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 // Don't worry about this
 module.exports = { name, height
+}
+if (name === "Susan") {
+    console.log("The name is correct")
 }
